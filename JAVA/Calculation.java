5ad06776421151b4f72d77c180f43ec666d1ceb9
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2023-02-17 17:35:27 +0900

    Add files via upload

--- JAVA/Calculation.java
+++ JAVA/Calculation.java
@@ -10,12 +10,36 @@ public class Calculation {
 		System.out.println("숫자 두 개를 입력해주세요");
 		int a = sc.nextInt();
 		int b = sc.nextInt();
+		System.out.println("어떤 연산을 하고 싶나요 + - * / %");
+		String op = sc.next();
+		int result=0;
 
-		System.out.println(a + " + " + b + " = " + (a + b));
-		System.out.println(a + " - " + b + " = " + (a - b));
-		System.out.println(a + " * " + b + " = " + (a * b));
-		System.out.println(a + " / " + b + " = " + (a / b));
-		System.out.println(a + " % " + b + " = " + (a % b));
+		switch (op) {
+		case "+":
+			result = a + b;
+			break;
+		case "-":
+			result = a - b;
+			break;
+		case "*":
+			result = a * b;
+			break;
+		case "/":
+			if (b == 0) {
+				System.out.println("0으로 나눌 수 없습니다.");
+				return;
+			}
+			result = a / b;
+			break;
+		case "%":
+			result = a % b;
+			break;
+		default:
+			System.out.println("올바른 연산자를 입력해주세요.");
+			return;
+		}
+		System.out.println(a + " " + op + " " + "=" + result);
+
 	}
 
 }
