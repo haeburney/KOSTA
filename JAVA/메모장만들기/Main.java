2b2dde19ef3932defa621ff1e4f04856c2b1fa53
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2023-03-03 17:48:59 +0900

    Create Main.java

---
+++ JAVA/메모장만들기/Main.java
@@ -0,0 +1,11 @@
+package Memo2;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Menu m = new Menu("hyunMemo/");
+		m.run(sc);
+	}
+}
