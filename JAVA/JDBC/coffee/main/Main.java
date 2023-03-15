2b77a100afd3c298367007a8aa5b2e2614c89c57
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2023-03-15 17:08:50 +0900

    Create Main.java

---
+++ JAVA/JDBC/coffee/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MainMenu menu = new MainMenu();
+		menu.run(new Scanner(System.in));
+	}
+
+}
