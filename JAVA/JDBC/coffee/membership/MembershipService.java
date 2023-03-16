e62031ccf811185cd6d99fdb1d09f0d37478f892
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2023-03-16 17:30:33 +0900

    Add files via upload

--- JAVA/JDBC/coffee/membership/MembershipService.java
+++ JAVA/JDBC/coffee/membership/MembershipService.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import member.MemberDao;
 import member.MemberService;
 import member.MemberVo;
+import menu.MenuVo;
 
 public class MembershipService {
 	MembershipDao membershipDao;
@@ -15,6 +16,7 @@ public class MembershipService {
 		this.memberDao = new MemberDao();
 	}
 
+	// 멤버십 정보 보여주기
 	public void selectAll() {
 		System.out.println("멤버십 정보 입니다.");
 		ArrayList<MembershipVo> list = membershipDao.selectAll();
@@ -23,13 +25,15 @@ public class MembershipService {
 		}
 	}
 
+	// 회원의 할인율 반환
 	public double selectRate() {
-		MemberVo memberVo = memberDao.select(MemberService.LOGINID);
-		MembershipVo membershipVo = membershipDao.select(memberVo.getGrade());
+		MembershipVo membershipVo = membershipDao.select(MemberService.LOGINID);
 
 		return membershipVo.getRate();
 	}
+	
 
+	// 회원의 멤버십 보여주기
 	public MembershipVo select(String id) {
 		MembershipVo vo = membershipDao.select(id);
 		return vo;
